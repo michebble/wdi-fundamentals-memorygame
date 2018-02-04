0933ebb5d904b385f2bb11a7ed71533e57d7ea81
Michael Hebblethwaite <mhhebble@gmail.com>
2018-02-04 16:10:09 +0900

    improved game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 console.log("Up and running!");
 
+
+// initilise variables
+
 var cards = [
 {
 rank: "queen",
@@ -22,38 +25,110 @@ suit: "diamonds",
 cardImage: "images/king-of-diamonds.png"
 }
 ];
-var cardsInPlay =[];
+
+var cardsInPlay = [];
+
+var cardsInPlayData = [];
+
+var totalScore = 0;
+
+
+
+// create functions
+
+var updateScore = function () {
+	var scoreTally = document.getElementById('score');
+	scoreTally.textContent = totalScore.toString();
+
+};
+
+var setNotification = function(message) {
+	var newNotification = document.getElementById('notification');
+	newNotification.textContent = message;
+};
 
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-				alert("You found a match!");
+				setNotification("You found a match!");
+				for (var i = 0; i < cardsInPlayData.length; i++) {
+					var returnCard = document.getElementById(cardsInPlayData[i]);
+					returnCard.removeEventListener('click', flipCard);
+				}
+				cardsInPlay = [];
+				cardsInPlayData = [];
+				totalScore = totalScore + 50;
+				updateScore();
 			} else {
-				alert("Sorry, try again!");
-			}
-		}
-}
+				setNotification("Sorry, try again!");
+				for (var i = 0; i < cardsInPlayData.length; i++) {
+					var returnCard = document.getElementById(cardsInPlayData[i]);
+					returnCard.setAttribute('src', 'images/back.png');
+				}
+				cardsInPlay = [];
+				cardsInPlayData = [];
+			};
+		};
+};
+
 
 var flipCard = function() {
 	var cardId = this.getAttribute('data-id');
 	this.setAttribute('src',cards[cardId].cardImage);
-	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+	//console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	checkForMatch();
+	//console.log(this.id);
+	cardsInPlayData.push(this.id);
+	//console.log(cards[cardId].cardImage);
+	//console.log(cards[cardId].suit);
+	setTimeout(checkForMatch, 2000);
+};
+
+var shuffle = function(array) {
+	// this based on the Fisher-Yates Shuffle
+	// found at https://bost.ocks.org/mike/shuffle/compare.html
+	// and moditfied to match learneds function creation. 
+	// I would like to learn more about licenses and how to attribute correctly.
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
 }
 
 var createBoard = function () {
-	for(i=0; i < cards.length; i += 1){
+	shuffle(cards);
+	for(i=0; i < cards.length; i += 1) {
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src','images/back.png');
 		cardElement.setAttribute('data-id',i);
-		cardElement.addEventListener('click',flipCard)
+		cardElement.setAttribute('id','card' + i);
+		cardElement.addEventListener('click', flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
 	}
 };
 
-createBoard();
+var resetBoard = function () {
+	var gameBoard = document.getElementById('game-board');
+		while (gameBoard.hasChildNodes()) {
+			gameBoard.removeChild(gameBoard.firstChild);
+	}
+	totalScore = 0;
+	updateScore();
+	setNotification("Ready to play?");
+	createBoard();
+};
 
+var createResetButton = function () {
+	var buttonElement = document.getElementById('reset');
+	buttonElement.addEventListener('click', resetBoard);
+}
+
+
+createBoard();
+createResetButton();
+updateScore();
+setNotification("Ready to play?");
 
